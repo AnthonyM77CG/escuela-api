a2c7b1bf02fb32e54a30a2984a6294364963ac91
Anthony Cotrina <anthony77CG@hotmail.com>
2025-08-11 22:51:07 -0500

    refactor: cambiar estrategia de generación de id de ALL a IDENTITY

--- src/main/java/com/school/api/entities/Rol.java
+++ src/main/java/com/school/api/entities/Rol.java
@@ -1,13 +1,10 @@
 package com.school.api.entities;
 
-import java.util.Set;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,12 +17,9 @@ import lombok.NoArgsConstructor;
 @Table(name = "db_roles")
 public class Rol {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false, unique = true)
     private String name;
-
-    @ManyToMany(mappedBy = "roles")
-    private Set<User> users;
 }
